3a8f33bf2357822b65142ea9a80756a084e229b9
furkan aydin <furknn2112@gmail.com>
2024-08-27 21:24:29 +0300

    feat: Add click functionality to basket icon
    - Get click event handler through props and bind it to basket icon's
    click event in Header component
    - Create state to control visibility of BasketSideBar component in
    Home component
    - Reference BasketSideBar from inside Home component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,18 @@
 import styles from './Header.module.css';
 import { BsFillBasketFill } from "react-icons/bs";
 
-const Header: React.FunctionComponent = () => {
+type props = {
+    shiftVisibilityOfBasketSidebar: () => void
+}
+
+const Header: React.FunctionComponent<props> = ({ shiftVisibilityOfBasketSidebar }) => {
 
     return <header className={styles.header}>
         <p className={styles.logo}>Furkan AYDIN</p>
         <div className={styles.searchInput}>
             <input type="text" placeholder='Search product' />
         </div>
-        <div className={styles.icon}>
+        <div className={styles.icon} onClick={shiftVisibilityOfBasketSidebar}>
             <BsFillBasketFill style={{ fontSize: '2.4rem' }} />
             <span className={styles.count}>0</span>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,23 @@
 import Header from "../components/header/Header";
+import ListProducts from "../components/list-products/ListProducts";
+import BasketSidebar from "../components/basket-sidebar/BasketSidebar";
+import { useState } from "react";
+
 
 const Home: React.FunctionComponent = () => {
+    // State to control visibility of BasketSidebar component
+    const [displayBasketSidebar, setDisplayBasketSidebar] = useState(false);
+
+    // Callback to trigger visibility of BasketSidebar component
+    function shiftVisibilityOfBasketSidebar(): void {
+        setDisplayBasketSidebar(!displayBasketSidebar);
+    }
 
     return <>
-        <Header />
-        <p>home page</p>
+        <Header shiftVisibilityOfBasketSidebar={shiftVisibilityOfBasketSidebar} />
+        <ListProducts />
+
+        {displayBasketSidebar && <BasketSidebar shiftVisibilityOfBasketSidebar={shiftVisibilityOfBasketSidebar} />}
     </>
 }
 
